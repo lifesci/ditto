64d8b9d672a16892958ed05056ef2d9eb6ce4453
lifesci <ericdito@gmail.com>
2023-03-28 13:52:29 -0700

    Continue working on grammar; add basic test case

--- src/ast.rs
+++ src/ast.rs
@@ -1,44 +1,86 @@
 #[derive(Debug)]
+pub struct DittoNode {
+    pub game: GameNode,
+    pub player: PlayerNode,
+}
+
+#[derive(Debug)]
 pub struct GameNode {
     pub name: String
 }
 
+#[derive(Debug)]
+pub struct PlayerNode {
+    pub name: String,
+    pub stats: Vec<StatNode>,
+    pub actions: Vec<ActionNode>,
+    pub triggers: Vec<TriggerNode>,
+}
+
+#[derive(Debug)]
+pub struct StatNode {
+    pub name: String,
+    pub val: Box<ExprEnum>,
+}
+
+#[derive(Debug)]
+pub struct ActionNode {
+    pub name: String,
+    pub targets: Vec<String>,
+    pub children: Vec<Box<StmtEnum>>,
+}
+
+#[derive(Debug)]
+pub struct TriggerNode {
+    pub name: String,
+    pub actions: Vec<String>,
+    pub children: Vec<Box<StmtEnum>>,
+}
+
+#[derive(Debug)]
 pub struct PropertyNode {
     pub var: String,
     pub prop: String,
 }
 
+#[derive(Debug)]
 pub struct WhileNode {
     pub cond: Box<ExprEnum>,
     pub children: Vec<Box<StmtEnum>>,
 }
 
+#[derive(Debug)]
 pub struct AssignNode {
     pub l: Box<AssignEnum>,
     pub r: Box<ExprEnum>,
 }
 
+#[derive(Debug)]
 pub struct DeclareNode {
     pub l: String,
     pub r: Box<ExprEnum>,
 }
 
+#[derive(Debug)]
 pub struct IfNode {
     pub cond: Box<ExprEnum>,
     pub children: Vec<Box<StmtEnum>>,
 }
 
+#[derive(Debug)]
 pub struct CondNode {
     pub main: IfNode,
     pub alt: Vec<IfNode>,
     pub fin: Vec<Box<StmtEnum>>,
 }
 
+#[derive(Debug)]
 pub enum AssignEnum {
     Var(String),
     Prop(PropertyNode),
 }
 
+#[derive(Debug)]
 pub enum StmtEnum {
     While(WhileNode),
     Assign(AssignNode),
@@ -46,17 +88,20 @@ pub enum StmtEnum {
     Cond(CondNode),
 }
 
+#[derive(Debug)]
 pub struct BinOpNode {
     pub op: BinOp,
     pub l: Box<ExprEnum>,
     pub r: Box<ExprEnum>,
 }
 
+#[derive(Debug)]
 pub struct UnaryOpNode {
     pub op: UnaryOp,
     pub l: Box<ExprEnum>,
 }
 
+#[derive(Debug)]
 pub enum ExprEnum {
     BinOp(BinOpNode),
     UnaryOp(UnaryOpNode),
@@ -68,6 +113,7 @@ pub enum ExprEnum {
     Prop(PropertyNode),
 }
 
+#[derive(Debug)]
 pub enum BinOp {
     Add,
     Sub,
@@ -83,6 +129,7 @@ pub enum BinOp {
     Lte,
 }
 
+#[derive(Debug)]
 pub enum UnaryOp {
     Neg,
     Not,
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 #[macro_use] extern crate lalrpop_util;
 
+use std::fs;
+
 lalrpop_mod!(pub ditto);
 
 mod ast;
 
 fn main() {
-    let val = ditto::GameParser::new().parse("game test").unwrap();
-    println!("{:?}", val);
+    match run_test() {
+        Ok(s) => {
+            let val = ditto::DittoParser::new().parse(&s).unwrap();
+            println!("{:?}", val);
+        },
+        Err(_e) => (),
+    }
 }
 
 #[test]
 fn ditto() {
-    assert!(ditto::GameParser::new().parse("game test").is_ok());
+    assert!(ditto::ExprParser::new().parse("2 + 2").is_ok());
+}
+
+fn run_test() -> Result<String, std::io::Error> {
+    let code = fs::read_to_string("src/test_scripts/test1.ditto");
+    return code;
 }
